a6a6f19606a1b9002aefcccf475faeac05165899
Fabio Pelosini <fabio.pelosini99@gmail.com>
2025-05-16 12:36:07 +0200

    lane increase speed over time

--- ZombieTsunami/Assets/Scripts/GameController.cs
+++ ZombieTsunami/Assets/Scripts/GameController.cs
@@ -1,24 +1,38 @@
+using System.Collections;
 using UnityEngine;
 
 public class GameController : Singleton<GameController>
 {
 
-    [SerializeField] float gameSpeedMultipler = 1f;
+    [SerializeField] Vector2 _gameSpeedRange = new Vector2(1f, 10f);
+    [SerializeField] float _timeToMaxSpeed = 0.1f;
+    float _gameSpeedMultipler = 1f;
+    float _timeFromStart = 0f;
 
     public float GameSpeedMultipler
     {
-        get { return gameSpeedMultipler; }
-        private set { gameSpeedMultipler = value; }
+        get { return _gameSpeedMultipler; }
+        private set { _gameSpeedMultipler = value; }
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-
+        // Start the coroutine to increase the game timer
+        StartCoroutine(IncreaseGameTimer());
     }
-
+    
     // Update is called once per frame
     void Update()
     {
+        GameSpeedMultipler = Mathf.Lerp(_gameSpeedRange.x, _gameSpeedRange.y, _timeFromStart / _timeToMaxSpeed);
+    }
 
+    IEnumerator IncreaseGameTimer()
+    {
+        while (true)
+        {
+            _timeFromStart += Time.deltaTime;
+            yield return null;
+        }
     }
 }
--- ZombieTsunami/Assets/Scripts/LaneMovement.cs
+++ ZombieTsunami/Assets/Scripts/LaneMovement.cs
@@ -1,12 +1,25 @@
+using System.Collections;
 using UnityEngine;
 
 public class LaneMovement : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
-    
-    private void Update()
+    [SerializeField] private float _speed = 5f;
+
+    private void Start()
     {
-        // Move the object to the left
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        // Start the movement coroutine
+        StartCoroutine(Move());
+    }
+
+    IEnumerator Move()
+    {
+        while (true)
+        {
+            // Set the speed of the object
+            float gameSpeed = GameController.Instance.GameSpeedMultipler;
+            // Move the object to the left
+            transform.Translate(Vector3.left * _speed * gameSpeed * Time.deltaTime);
+            yield return null;
+        }
     }
 }
